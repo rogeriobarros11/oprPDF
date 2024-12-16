b977a9d75a7fac254a0fbe1ab7568a8bba1a1f4c
rogeriobarros11 <112919030+rogeriobarros11@users.noreply.github.com>
2024-12-16 12:32:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
-    $("#flipbook").turn({
-        width: 800,
-        height: 500,
-        autoCenter: true
-    });
+$(document).ready(function () {
+    if ($.isFunction($.fn.turn)) {
+        $('#flipbook').turn({
+            width: 800,
+            height: 500,
+            autoCenter: true,
+        });
+    } else {
+        console.error('Turn.js não foi carregado corretamente.');
+    }
 });
